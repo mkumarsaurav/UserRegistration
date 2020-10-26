a4e2401d2e33e8a9ca4ab31fc50cc1c55fe1faa9
saurav <kumar00914@gmail.com>
2020-10-26 15:41:57 +0530

    Added: Pattern to validate first name which starts with capital letter and must contain atleast 3 characters

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to User Registration Validation"
+#first_name validation
+read -p "Enter First Name: " fname;
+fname_pattern="^[A-Z][a-zA-Z]{2,}$";
+if [[ $fname =~ $fname_pattern ]]
+then
+	echo "Valid First Name";
+else
+	echo "Invalid First Name";
+fi
