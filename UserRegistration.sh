02ad77970f490a10fe0d8fafe3fc775afb113a3a
saurav <kumar00914@gmail.com>
2020-10-27 13:40:00 +0530

    Added: Validation of last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,10 +2,19 @@
 echo "Welcome to User Registration Validation"
 #first_name validation
 read -p "Enter First Name: " fname;
-fname_pattern="^[A-Z][a-zA-Z]{2,}$";
-if [[ $fname =~ $fname_pattern ]]
+pattern="^[A-Z][a-zA-Z]{2,}$"
+if [[ $fname =~ $pattern ]]
 then
 	echo "Valid First Name";
 else
 	echo "Invalid First Name";
+fi
+
+#last_name validation
+read -p "Enter Last Name: " lname;
+if [[ $lname =~ $pattern ]]
+then
+   echo "Valid Last Name";
+else
+   echo "Invalid Last Name";
 fi
